7466b7975fbff74af515a27f2a82f316b2df7a2c
naren <narendervenna@gmail.com>
2024-10-05 10:24:24 -0500

    code

---
+++ src/main/java/com/example/spring_boot_github_cicd/SpringBootGithubCicdApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_boot_github_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SpringBootGithubCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_github_cicd/SpringBootGithubCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_github_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
